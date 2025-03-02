5c94e4bfc191c3b95ed803e9e322417429d9e85e
nyankiti <nowi41cic77mav@gmail.com>
2025-03-03 01:40:53 +0900

    updateViewをfrontMatter利用に変更

--- batch/updateViews.ts
+++ batch/updateViews.ts
@@ -1,57 +1,36 @@
 import * as core from "@actions/core";
 import { GaApiClient } from "./libs/ga-client";
 import { writeFile, readFile } from "node:fs/promises";
+import matter from "gray-matter";
+import path from "path";
 
 export async function run() {
   const gaApiClient = new GaApiClient();
-
   const pvMap = await gaApiClient.getPv();
 
   const contentsPath = process.env.GITHUB_WORKSPACE
     ? process.env.GITHUB_WORKSPACE + "/contents/tech-blog"
     : "/home/runner/work/blog-contents/contents/tech-blog";
 
-  // FrontMatterを抽出するための正規表現
-  const metadataRegex = /^---\n([\s\S]*?)\n---\n/m;
-
   for (const slug in pvMap) {
-    const filePath = `${contentsPath}/${slug}.md`;
+    const filePath = path.join(contentsPath, `${slug}.md`);
 
     try {
       const content = await readFile(filePath, "utf-8");
 
-      const match = content.match(metadataRegex);
-      let metadata: Record<string, any> = {};
-      if (match) {
-        const metadataString = match[1];
-        metadata = metadataString
-          .split("\n")
-          .reduce((acc: Record<string, string>, line) => {
-            const [key, value] = line.split(": ");
-            acc[key] = value;
-            return acc;
-          }, {});
-      }
+      const file = matter(content);
 
       // views数が更新されていない場合はファイル書き込みをスキップする
-      if (metadata["views"] == pvMap[slug])
-        throw Error(`skip because of ${slug} page views is not changed`);
+      if (file.data.views === pvMap[slug]) {
+        core.debug(`Skipping ${slug}: page views unchanged (${pvMap[slug]})`);
+        continue;
+      }
 
-      // metadataにviewsを追加
-      metadata["views"] = pvMap[slug];
+      file.data.views = pvMap[slug];
 
-      // 新しいメタデータ文字列を作成
-      const newMetadataString = Object.entries(metadata)
-        .map(([key, value]) => `${key}: ${value}`)
-        .join("\n");
+      const updatedContent = matter.stringify(file.content, file.data);
 
-      // ファイルの内容を更新
-      const newContent = content.replace(
-        metadataRegex,
-        `---\n${newMetadataString}\n---\n`
-      );
-      await writeFile(filePath, newContent);
-
+      await writeFile(filePath, updatedContent);
       core.info(`Successfully updated ${slug} views`);
     } catch (err) {
       // ファイルが存在しない、view数の変化がない場合などはこちらのエラーに入る
