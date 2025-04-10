c4560712212b4b811a50aa77fb4c45668232f5e7
nyankiti <nowi41cic77mav@gmail.com>
2025-04-11 00:03:29 +0900

    make en dist dir

--- batch/generatePagesArtifact.ts
+++ batch/generatePagesArtifact.ts
@@ -6,10 +6,12 @@ import {
 } from "./libs/posts";
 import { generateSearchIndex } from "./libs/generate-search-index";
 import { generateGourmetPostsJson } from "./libs/gourmet";
+import path from "node:path";
 
 const main = async () => {
   // dist ディレクトリが存在しない場合は作成
   await mkdir(distDir, { recursive: true });
+  await mkdir(path.join(distDir, "en"), { recursive: true });
   // テックブログの記事一覧 JSON の生成
   await generateTechBlogPostsJson();
   await generateEnglishTechBlogPostsJson();
